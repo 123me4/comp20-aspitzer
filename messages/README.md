87d52f7bded8dd105aa6a15061a37cc9d0a8fca8
Alex Spitzer <spitzer.alex@gmail.com>
2018-06-20 21:43:35 -0400

    Working parse and style

---
+++ messages/README.md
@@ -0,0 +1,12 @@
+# Alex Spitzer Lab 9
+### Completeness
+I was able to get everything to work.
+
+### Help
+I had help from my dad again.  He helped me figure out why my mark styling wasn't updating.  Turns out, cashe.
+
+### Time
+This assignment took me 2 hours (yes, too long).
+
+### is it possible to request the data from a different origin or from your local machine from using XMLHttpRequest?
+No.  XMLHttpRequest does not support cross origin requests.  This is because of the same-origin policy.
--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,12 @@
 // Your JavaScript goes here...
+parse = function() {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+	    if (this.readyState == 4 && this.status == 200) {
+	        var myArr = JSON.parse(this.responseText);
+	        document.getElementById("messages").innerHTML = "<mark>" + myArr[0].username + ": " + myArr[0].content + "</mark><br><br><mark>" + myArr[1].username + ": " + myArr[1].content + "</mark>";
+	    }
+	};
+	xmlhttp.open("GET", "data.json", true);
+	xmlhttp.send();
+}
